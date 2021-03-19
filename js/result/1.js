fa0c46b92382fe6eb2cdbed358242f8b1123a0aa
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-03-19 10:17:20 -0300

    Update 1.js

--- js/result/1.js
+++ js/result/1.js
@@ -1,19 +1,28 @@
 const OK = Symbol('Result.ok');
 const ERR = Symbol('Result.err');
-const $type = Symbol('Result.$type');
-const $data = Symbol('Result.$data');
 
-const resultPrototype = {
+class Result {
+  #type;
+  #data;
+
+  constructor(type, data) {
+    if (type !== OK && type !== ERR) {
+      throw new Error('Invalid Result type.');
+    }
+    this.#type = type;
+    this.#data = data;
+  }
+
   // Checks if the Result is the ok variant.
   isOk() {
-    return this[$type] === OK;
-  },
-  
+    return this.#type === OK;
+  }
+
   // Checks if the Result is the err variant.
   isErr() {
-    return this[$type] === ERR;
-  },
-  
+    return this.#type === ERR;
+  }
+
   // Creates a new Result type by mapping the ok variant.
   // Error variant is ignored (although a new Result is also created).
   map(fn) {
@@ -21,46 +30,43 @@ const resultPrototype = {
       ok: (data) => ok(fn(data)),
       err: (error) => err(error)
     });
-  },
-  
+  }
+
   // Throws an `Error` with `message` if the Result is the err variant.
   // Returns the wrapped value if the Result is the ok variant.
   expect(message) {
     return this.match({
       ok: (data) => data,
       err: () => {
-        throw new Error(message)
+        throw new Error(message);
       }
     });
-  },
-  
+  }
+
   // Throws if the Result is err. Otherwise, returns the wrapped value.
   unwrap() {
     return this.expect('Cannot unwrap err Result variant');
-  },
-  
+  }
+
   // Match the result variant, executing a callback function and returning the value.
   match(matchMap) {
-    switch (this[$type]) {
-      case OK: return matchMap.ok(this[$data]);
-      case ERR: return matchMap.err(this[$data]);
-      default: throw new Error('Unreachable; invalid Result type');
+    switch (this.#type) {
+      case OK:
+        return matchMap.ok(this.#data);
+      case ERR:
+        return matchMap.err(this.#data);
+      default:
+        throw new Error('Unreachable; invalid Result type');
     }
   }
-};
+}
 
 // Creates a new ok variant Result.
 function ok(val) {
-  return Object.assign(Object.create(resultPrototype), {
-    [$type]: OK,
-    [$data]: val
-  });
+  return new Result(OK, val);
 }
 
 // Creates a new err variant Result.
 function err(val) {
-  return Object.assign(Object.create(resultPrototype), {
-    [$type]: ERR,
-    [$data]: val
-  });
+  return new Result(ERR, val);
 }
