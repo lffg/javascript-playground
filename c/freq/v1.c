1b00df6ce188d98087079219fe0301d7d6f3b9e6
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-05-12 22:20:29 -0300

    Create v1.c

---
+++ c/freq/v1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int freq = 0;
+
+  int target;
+  scanf("%u", &target);
+
+  while (1)
+  {
+    int current;
+    scanf("%u", &current);
+
+    if (current == 0)
+      break;
+    if (current == target)
+      freq++;
+  }
+
+  printf("%u\n", freq);
+}
