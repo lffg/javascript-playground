3dd3bdb56a3073bf1ae26b56e7c34727dcf7ce15
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-05-20 07:56:19 -0300

    Create parser.js

---
+++ js/projects/q-randomizer/parser.js
@@ -0,0 +1,26 @@
+const IMAGE_IDENTIFICATION_REGEX = /^(?<front>[ABC])(?<module>\d{2})-(?<qKind>[APE])(?<qNum>\d{2})-(?:(?<qSimpleAnswer>[A-E])|\((?<qComplexAnswer>[^\)]+)\))$/;
+const SCAPEABLE_COMMA_SPLITTER_REGEX = /(?<!\\),/;
+
+function parseImgName(str) {
+  // Example name:
+  // C03-P05-B             :: [simple  qAnswer]
+  // B01-A10-(2\,5m, 5s)   :: [complex qAnswer]
+
+  const match = str.match(IMAGE_IDENTIFICATION_REGEX);
+  if (!match) {
+    throw new TypeError('Invalid image identification name.');
+  }
+
+  const { qSimpleAnswer, qComplexAnswer, ...base } = match.groups;
+
+  const qAnswer = {
+    kind: qSimpleAnswer ? 'simple' : 'complex',
+    answer: qSimpleAnswer
+      ? qSimpleAnswer
+      : qComplexAnswer
+          .split(SCAPEABLE_COMMA_SPLITTER_REGEX)
+          .map((str) => str.replaceAll('\\,', ','))
+  };
+
+  return { ...base, qAnswer };
+}
