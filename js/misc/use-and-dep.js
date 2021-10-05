db5ff6ab5dae7e8af110aeb358a2f9c022e23b85
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-10-05 10:35:05 -0300

    Create use-and-dep.js

---
+++ js/misc/use-and-dep.js
@@ -0,0 +1,56 @@
+const warn = console.warn.bind(console);
+const error = console.error.bind(console);
+
+const USE_DECL = Symbol('use declaration');
+const DEP_DECL = Symbol('dependency declaration');
+
+const Use = (ident, fn) => ({ kind: USE_DECL, ident, fn });
+const Dep = (ident) => ({ kind: DEP_DECL, ident });
+
+function analyzeList(list) {
+  const unusedIdents = new Set();
+  const globalIdents = new Map();
+  const deps = new Set();
+  for (const item of list) {
+    switch (item.kind) {
+      case USE_DECL: {
+        globalIdents.set(item.ident, item.fn);
+        unusedIdents.add(item.ident);
+        break;
+      }
+      case DEP_DECL: {
+        deps.add(item.ident);
+        break;
+      }
+      default: {
+        throw new Error('Unreachable.');
+      }
+    }
+  }
+  for (const ident of deps) {
+    if (!globalIdents.has(ident)) {
+      error(`Reference error: Use of undeclared variable '${ident}'.`);
+      continue;
+    }
+    if (unusedIdents.has(ident)) {
+      unusedIdents.delete(ident);
+    }
+    globalIdents.get(ident)();
+  }
+  if (unusedIdents.size !== 0) {
+    for (const ident of unusedIdents) {
+      warn(`Unused declared variable '${ident}'.`);
+    }
+  }
+}
+
+const itemList = [
+  Dep('b'),
+  Use('a', () => console.log('a called!')),
+  Dep('b'),
+  Use('c', () => console.log('c called!')),
+  Use('b', () => console.log('b called!')),
+  Dep('a')
+];
+
+analyzeList(itemList);
