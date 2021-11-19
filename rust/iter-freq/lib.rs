1fef026d7797b5c39c71cf08e6c338cbec98a33d
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-11-19 08:59:33 -0300

    Add fn alternative

--- rust/iter-freq/lib.rs
+++ rust/iter-freq/lib.rs
@@ -1,17 +1,36 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
-pub trait Freq<T: Eq + Hash>: Iterator {
-    fn freq(self) -> HashMap<T, usize>;
+pub mod ext {
+    use super::*;
+
+    pub trait Freq<T: Eq + Hash>: Iterator {
+        fn freq(self) -> HashMap<T, usize>;
+    }
+
+    impl<I, K> Freq<K> for I
+    where
+        I: Iterator<Item = K>,
+        K: Eq + Hash,
+    {
+        fn freq(self) -> HashMap<K, usize> {
+            self.fold(HashMap::new(), |mut map, curr| {
+                *map.entry(curr).or_insert(0) += 1;
+                map
+            })
+        }
+    }
 }
 
-impl<I, T> Freq<T> for I
-where
-    I: Iterator<Item = T>,
-    T: Eq + Hash,
-{
-    fn freq(self) -> HashMap<T, usize> {
-        self.fold(HashMap::new(), |mut map, curr| {
+pub mod r#fn {
+    use super::*;
+
+    pub fn freq<I, K>(iter: I) -> HashMap<K, usize>
+    where
+        I: Iterator<Item = K>,
+        K: Eq + Hash,
+    {
+        iter.fold(HashMap::new(), |mut map, curr| {
             *map.entry(curr).or_insert(0) += 1;
             map
         })
@@ -22,11 +41,17 @@ where
 mod tests {
     use std::collections::HashMap;
 
-    use super::Freq;
+    use super::{ext::Freq, r#fn::freq};
 
     #[test]
-    fn test_1() {
+    fn test_freq_extension_trait() {
         let freq = "AAABBC".chars().freq();
-        assert_eq!(freq, HashMap::from([('A', 3), ('B', 2), ('C', 1)]))
+        assert_eq!(freq, HashMap::from([('A', 3), ('B', 2), ('C', 1)]));
+    }
+
+    #[test]
+    fn test_freq_fn() {
+        let iter = "AAABBC".chars();
+        assert_eq!(freq(iter), HashMap::from([('A', 3), ('B', 2), ('C', 1)]));
     }
 }
