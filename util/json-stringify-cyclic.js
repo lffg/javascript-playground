c62831984a593f5769e3f59944f85996c761db99
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-12-05 01:16:42 -0300

    Add json-stringify-cyclic

---
+++ util/json-stringify-cyclic.js
@@ -0,0 +1,33 @@
+function createReplacer(nameFn) {
+  const seen = new WeakSet();
+  return function replacer(key, val) {
+    if (typeof val === 'object' && !!val) {
+      if (seen.has(val)) {
+        return nameFn(val);
+      }
+      seen.add(val);
+    }
+    return val;
+  };
+}
+
+function stringifyCyclic(startNode, space, nameFn) {
+  if (typeof nameFn !== 'function') {
+    // eslint-disable-next-line no-param-reassign
+    nameFn = (obj) => `[cycle: ${Object.prototype.toString.call(obj)}]`;
+  }
+
+  const replacer = createReplacer(nameFn);
+  return JSON.stringify(startNode, replacer, space);
+}
+
+//
+// Example usage:
+//
+
+const nodeC = { value: 'c', next: null };
+const nodeB = { value: 'b', next: nodeC };
+const nodeA = { value: 'a', next: nodeB };
+nodeC.next = nodeA;
+
+console.log(stringifyCyclic(nodeA, 2, (o) => `[cycle: node(${o.value})]`));
