5708897301c7af19d7960ff6af4d7cc7bc5798c1
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2022-09-13 22:15:43 -0300

    Create queue.js

---
+++ ds/queue.js
@@ -0,0 +1,51 @@
+class Queue {
+  #data = [];
+
+  #cap = 0;
+  #len = 0;
+
+  #first = 0;
+  #next = 0;
+
+  // Initializes a new Queue with the provided capacity.
+  constructor(cap) {
+    this.#cap = cap;
+  }
+
+  // Inserts element at the end, O(1).
+  insert(el) {
+    if (this.#len == this.#cap) {
+      throw new Error("Not enough capacity");
+    }
+    this.#len++;
+    this.#data[this.#next] = el;
+    this.#next = (this.#next + 1) % this.#cap;
+  }
+
+  // Removes the first element, O(1).
+  remove() {
+    if (!this.#len) {
+      throw new Error("Already empty");
+    }
+    this.#len--;
+    const old = this.#data[this.#first];
+    this.#data[this.#first] = null; // This isn't needed.
+    this.#first = (this.#first + 1) % this.#cap;
+    return old;
+  }
+
+  // Applies `cb` for each element, O(n).
+  forEach(cb) {
+    let i = this.#first;
+    while (i !== this.#next) {
+      cb(this.#data[i]);
+      i = (i + 1) % this.#cap;
+    }
+  }
+
+  // Returns shallow a copy of the internal buffer, O(n).
+  // For debugging purposes.
+  get $data() {
+    return [...this.#data];
+  }
+}
